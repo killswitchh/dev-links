eb9671db474a726cdd428da3c6c29759423ddf25
Balaji <balaji110499@gmail>
2023-03-27 01:12:10 +0530

    feat: initiated wizard store

---
+++ src/components/Wizard/CreatePageWizardPreview.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<div class="w-[30%] border-l-amber-900 flex flex-row justify-center">
+  <div
+    class="border-black rounded-3xl border-2 ml-4 mr-4 mt-20 h-2/3 w-3/4 bg-yellow-300 flex flex-row justify-center"
+  >
+    <div>Mobile Preview</div>
+  </div>
+</div>
--- src/stores.ts
+++ src/stores.ts
@@ -4,11 +4,42 @@ import { writable } from 'svelte/store'
 export type Store = {
   error?: string | null
   user?: Session | null
+  wizard?: WizardStep[]
+}
+
+export type WizardStep = {
+  stepId: number
+  title: string
+  status: 'INITIATED' | 'IN-PROGRESS' | 'COMPLETE'
+  value: any // Link[] | Style
 }
 
 const defaultStoreValue: Store = {
   error: null,
+  wizard: [
+    {
+      stepId: 1,
+      title: 'Links',
+      status: 'INITIATED',
+      value: {},
+    },
+    {
+      stepId: 2,
+      title: 'Buttons',
+      status: 'INITIATED',
+      value: {},
+    },
+    {
+      stepId: 3,
+      title: 'Theme',
+      status: 'INITIATED',
+      value: {},
+    },
+  ],
 }
+
+export type WizardStepStatus = 'INITIATED' | 'IN-PROGRESS' | 'COMPLETE'
+
 function createAppStore() {
   const { subscribe, update } = writable(defaultStoreValue)
 
@@ -22,6 +53,24 @@ function createAppStore() {
     updateCurrentSession: (user: Session | null) =>
       update((store) => {
         store.user = user
+        return store
+      }),
+    updateWizardStepValue: (stepId: number, value: any) =>
+      update((store) => {
+        store.wizard?.map((wiz) => {
+          if (wiz.stepId === stepId) {
+            wiz.value = value
+          }
+        })
+        return store
+      }),
+    updateWizardStepStatus: (stepId: number, status: WizardStepStatus) =>
+      update((store) => {
+        store.wizard?.map((wiz) => {
+          if (wiz.stepId === stepId) {
+            wiz.status = status
+          }
+        })
         return store
       }),
   }
