6e8a7053944de9f5ddb649f3157e86188736fd72
Balaji <balaji110499@gmail>
2023-04-12 02:48:48 +0530

    fix: create link

--- src/constants.ts
+++ src/constants.ts
@@ -1,16 +1,7 @@
-import { PUBLIC_API_URL } from '$env/static/public';
-
 export const CONSTANTS = {
   EMPTY_STRING: '',
   GITHUB_ERROR_MESSAGES: {
     NOT_FOUND: 'User has no repositories',
-  },
-  API_URL: {
-    LINK_GROUP_BASE_URL: `${PUBLIC_API_URL}link-groups`,
-    USERS_BASE_URL: `${PUBLIC_API_URL}users`,
-    LINKS_BASE_URL: `${PUBLIC_API_URL}links`,
-    THEMES_BASE_URL: `${PUBLIC_API_URL}themes`,
-    METADATA_BASE_URL: `${PUBLIC_API_URL}metadata`,
   },
 };
 
